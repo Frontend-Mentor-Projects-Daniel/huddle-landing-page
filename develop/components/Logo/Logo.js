bb05ceaf59836536b1ce6c8195c70072a728b34c
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-08-26 11:21:06 -0400

    create and style logo component and make it responsive for all screen sizes and add mobile and desktop background images to body

---
+++ develop/components/Logo/Logo.js
@@ -0,0 +1,21 @@
+fetch('./develop/components/Logo/Logo.html')
+  .then((stream) => stream.text())
+  .then((text) => {
+    define(text);
+  });
+
+function define(html) {
+  class Logo extends HTMLElement {
+    constructor() {
+      super();
+
+      const shadow = this.attachShadow({ mode: 'open' });
+      shadow.innerHTML = html;
+    }
+
+    // VARIABLES
+
+    // METHODS
+  }
+  window.customElements.define('logo-component', Logo);
+}
